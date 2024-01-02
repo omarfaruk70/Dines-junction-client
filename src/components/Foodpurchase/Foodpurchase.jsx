78bf64f7997c283cf119cd9df7ae2fb970515fda
omarfaruk70 <faruk1611281892@gmail.com>
2024-01-02 22:06:22 +0600

    removed unused code from foodpurchase page

--- src/components/Foodpurchase/Foodpurchase.jsx
+++ src/components/Foodpurchase/Foodpurchase.jsx
@@ -14,13 +14,12 @@ const Foodpurchase = () => {
     const form = event.target;
     const foodName = form.food_name.value;
     const foodImage = form.food_image.value;
-    const foodOwner = form.food_Owner.value;
     const foodPrice = form.price.value;
     const foodQuantity = form.quantity.value;
     const buyerName = form.buyer_name.value;
     const buyerEmail = form.buyer_email.value;
     const buyingDate = form.buying_date.value;
-    const purchaseFood = {foodName, foodImage, foodOwner, foodPrice, foodQuantity, buyerName, buyerEmail, buyingDate};
+    const purchaseFood = {foodName, foodImage, foodPrice, foodQuantity, buyerName, buyerEmail, buyingDate};
     if(foodPrice && foodQuantity < 1){
       return   Swal.fire({
         icon: "error",
@@ -77,23 +76,6 @@ const Foodpurchase = () => {
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
            Food Image
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="text"
-            name="food_Owner"
-            id="food_Owner"
-            defaultValue={madeBy}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="food_Owner"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-           Food Owner
           </label>
         </div>
         <div className="relative z-0 w-full mb-5 group">
