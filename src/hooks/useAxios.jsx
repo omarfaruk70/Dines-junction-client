722257f8d9b940352f7f616f5915c5f672fca738
omarfaruk70 <faruk1611281892@gmail.com>
2024-01-02 12:02:55 +0600

    added jwt interceptor

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,12 +1,24 @@
 import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Providers/AuthProviders";
 
+const axiosInstance = axios.create(
+    {
+        baseURL: 'http://localhost:5000/api/v1',
+        withCredentials: true
+    }
+)
 const useAxios = () => {
-    const axiosInstance = axios.create(
-        {
-            baseURL: 'http://localhost:5000/api/v1',
-            withCredentials: true
+    const {logOutUser} = useContext(AuthContext);
+    useEffect( () => { axiosInstance.interceptors.response.use(
+        function (response){
+            return response;
+        }, function (error){
+            if(error?.response?.status === 401 || error?.response?.status === 403){
+                logOutUser();
+            }
         }
-    )
+    )}, [])
     return axiosInstance;
 };
 
